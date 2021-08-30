f0d9392bd362fa456409ed26605e0b7d9acb041d
RamanSB <ramansb100@gmail.com>
2021-08-30 17:58:59 +0100

    using path

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 dotenv.config();
 const app = express();
